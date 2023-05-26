e4ddfdafb1ae54e40bb54b83460db56c21e59e82
srwesteban <sr.w@hotmail.com>
2023-05-25 23:00:23 -0500

    versionfinal

--- cajeroW/Usuarios.py
+++ cajeroW/Usuarios.py
@@ -14,7 +14,6 @@ class Usuario:
                     if usuario == self.Cuenta and contrasena_archivo == self.Contrasena:
                         return nombre
         except FileNotFoundError:
-
             return None
 
     def validar_usuario(self, nombre):
@@ -22,6 +21,5 @@ class Usuario:
             sys.exit('Datos incorrectos fin del programa')
 
     def enviar_usuario(self, nombre):
-        nombres = ['ana', 'andrea', 'benito', 'carlos', 'daniel', 'jaime', 'jesus', 'juan', 'lorena', 'mercedes',
-                   'pablo', 'pedro', 'sandra', 'sebastian']
+        nombres = ['ana', 'andrea', 'benito', 'carlos', 'daniel', 'jaime', 'jesus', 'juan', 'lorena', 'mercedes','pablo', 'pedro', 'sandra', 'sebastian']
         return nombres.index(nombre) if nombre in nombres else 14
